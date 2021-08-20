af0782b08acd53cef35e0089b2cfcf32ecb8d715
Ronaldo Marino <ronaldobmteixeira@gmail.com>
2021-08-20 19:20:16 -0300

    Aula sobre if e when

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,30 @@
 fun main(){
-    print("Bem vindo ao ByteBank")
+    println("Bem vindo ao ByteBank")
+
+    val titular = "Ronaldo"
+    val conta = 1000
+    var saldo = 0.0
+
+    saldo = 100.0
+
+    saldo -= 100.1
+
+    println("Titular: $titular")
+    println("Conta: $conta")
+    println("Saldo: $saldo")
+
+    if (saldo > 0.0){
+        println("Saldo é positivo")
+    } else if(saldo == 0.0){
+        println("Saldo é neutro")
+    } else {
+        println("Saldo é negativo")
+    }
+
+    when {
+        saldo > 0.0 -> println("Saldo é positivo")
+        saldo == 0.0 -> println("Saldo é neutro")
+        else -> println("Saldo é negativo")
+    }
+
 }
